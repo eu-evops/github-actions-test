fc533b1f4539c8f4c3afaf24256c32da3d5d62b2
Stanislaw Wozniak <swozniak@evops.eu>
2022-01-14 17:48:34 +0000

    Add p.onexit

--- .github/actions/js/index.js
+++ .github/actions/js/index.js
@@ -7,6 +7,11 @@ const child_process = require('child_process');
 const p = child_process.spawn('bash', ['-c', 'trap "echo BASHSIGINT" SIGINT; trap "echo BASHSIGTERM" SIGTERM; echo Hello World from bash; sleep 60'], { detached: true })
 p.stdout.on('data', m => console.log('STDOUT: %s', m))
 p.stderr.on('data', m => console.log('STDERR: %s', m))
+
+p.on('exit', (code, signal) => {
+  console.log('EXIT', code, signal)
+})
+
 p.addListener('message', m => console.log('MESSAGE: %s', m))
 p.addListener('exit', (code, signal) => console.log('EXIT', code, signal))
 p.addListener('close', code => console.log('CLOSE: %s', code))
