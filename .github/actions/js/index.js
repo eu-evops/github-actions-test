d7de9e82b4866fed71ded2cb1f880d94a0ba1833
Stanislaw Wozniak <swozniak@evops.eu>
2022-01-14 15:45:53 +0000

    Adding signal handling

--- .github/actions/js/index.js
+++ .github/actions/js/index.js
@@ -4,12 +4,22 @@ const core = require('@actions/core')
 const process = require('process');
 const child_process = require('child_process');
 
+
+const p = child_process.spawn('bash', ['-c', 'trap "echo BASHSIGINT" SIGINT; trap "echo BASHSIGTERM" SIGTERM; echo Hello World from bash; sleep 60'])
+p.stdout.on('data', m => console.log('STDOUT: %s', m))
+p.stderr.on('data', m => console.log('STDERR: %s', m))
+p.addListener('message', console.log)
+
+
+
 process.addListener('SIGINT', () => {
   console.log('Hello World from SIGINT');
+  p.kill('SIGINT');
 })
 
 process.addListener('SIGTERM', () => {
   console.log('Hello World from SIGTERM');
+  p.kill('SIGTERM');
 })
 
 // exec.exec('bash', [
@@ -18,8 +28,3 @@ process.addListener('SIGTERM', () => {
 // ], {
 
 // })
-
-const p = child_process.spawn('bash', ['-c', 'trap "echo BASHSIGINT" SIGINT; trap "echo BASHSIGTERM" SIGTERM; echo Hello World from bash; sleep 60'])
-p.stdout.on('data', m => console.log('STDOUT: %s', m))
-p.stderr.on('data', m => console.log('STDERR: %s', m))
-p.addListener('message', console.log)
