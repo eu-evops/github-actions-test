0118aa9c4d77e0020b41df7a9aea9d54bd96f990
Stanislaw Wozniak <swozniak@evops.eu>
2022-01-14 14:53:29 +0000

    minimal

--- .github/actions/js/index.js
+++ .github/actions/js/index.js
@@ -1,14 +1,5 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const process = require('process');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+process.addListener('SIGINT', () => {
+  console.log('Hello World from SIGINT');
+})
