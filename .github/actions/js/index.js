bd82f32ea92cbfe753935fa136460bac86383cfb
Stanislaw Wozniak <swozniak@evops.eu>
2022-01-14 15:48:31 +0000

    asdf

--- .github/actions/js/index.js
+++ .github/actions/js/index.js
@@ -8,7 +8,11 @@ const child_process = require('child_process');
 const p = child_process.spawn('bash', ['-c', 'trap "echo BASHSIGINT" SIGINT; trap "echo BASHSIGTERM" SIGTERM; echo Hello World from bash; sleep 60'])
 p.stdout.on('data', m => console.log('STDOUT: %s', m))
 p.stderr.on('data', m => console.log('STDERR: %s', m))
-p.addListener('message', console.log)
+p.addListener('message', m => console.log('MESSAGE: %s', m))
+p.addListener('exit', code => console.log('EXIT: %s', code))
+p.addListener('close', code => console.log('CLOSE: %s', code))
+p.addListener('error', code => console.log('ERROR: %s', code))
+p.addListener('spawn', code => console.log('SPAWN: %s', code))
 
 
 
