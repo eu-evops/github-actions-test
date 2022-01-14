95255d1636caeacc12b87c68afac7fc66e5e4028
Stanislaw Wozniak <swozniak@evops.eu>
2022-01-14 17:54:45 +0000

    asdasd

--- .github/actions/js/index.js
+++ .github/actions/js/index.js
@@ -4,7 +4,7 @@ const core = require('@actions/core')
 const process = require('process');
 const child_process = require('child_process');
 
-const p = child_process.spawn('bash', ['-c', 'trap "echo BASHSIGINT" SIGINT; trap "echo BASHSIGTERM" SIGTERM; echo Hello World from bash; sleep 60'])
+const p = child_process.spawn('bash', ['-c', 'echo Hello World from bash; sleep 60'])
 p.stdout.on('data', m => console.log('STDOUT: %s', m))
 p.stderr.on('data', m => console.log('STDERR: %s', m))
 
